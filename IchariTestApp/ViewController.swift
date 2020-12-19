3de134d8959223a52aa263835ec09dca0dd224a8
sherlockian4869 <ya0813kendo@gmail.com>
2020-12-19 13:45:28 +0900

    Initial Commit

---
+++ IchariTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IchariTestApp
+//
+//  Created by Kohei Yaeo on 2020/12/19.
+//  Copyright © 2020 sherlockian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
